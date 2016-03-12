64716931087980da0650d12637c2bc28af826989
sashablokhin <sashablokhindev@gmail.com>
2016-03-12 20:19:54 +0300

    Added auth and feed view controllers

---
+++ VKClone/AuthViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AuthViewController.swift
+//  VKClone
+//
+//  Created by Alexander Blokhin on 12.03.16.
+//  Copyright © 2016 Alexander Blokhin. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
